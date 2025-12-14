67d10ac2f0e2267d9ad5087ced7c5610bddd81f3
ZafriHakimi <zafrihakimi@gmail.com>
2025-12-14 16:21:38 +0800

    initialize project

---
+++ Money-tracker/backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/test', (req, res) => {
+    res.send('test ok 2');
+})
+
+app.listen(5000, () => {
+    console.log('Server running on http://localhost:5000')
+})
---
+++ Money-tracker/frontend/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const [desc, setDesc] = useState('')
+
+  const addTransaction = () => {}
+
+  return (
+    <main>
+      <h1>RM500<span>.00</span></h1>
+      <form onSubmit={addTransaction}>
+        <div className='basic'>
+          <input 
+            type='text' 
+            value={name} 
+            placeholder=''
+            onChange={e => setName(e.target.value)}
+          />
+          <input 
+            type='datetime-local'
+            value={date}
+            onChange={e => setDate(e.target.value)}
+          />
+        </div>
+        <div className='description'>
+          <input 
+            type='text' 
+            placeholder='description'
+            value={desc}
+            onChange={e => setDesc(e.target.value)}
+          />
+        </div>
+        <button type='submit'>Add new transaction</button>
+      </form>
+
+      <div className="transaction">
+
+        {/* Transaction 1 */}
+        <div className="left">
+          <div className="name">Samsung TV</div>
+          <div className="description"></div>
+        </div>
+        <div className="right">
+          <div className="price red">-RM500</div>
+          <div className="datetime">2/12/2025 15:30</div>
+        </div>
+
+      </div>
+
+      <div className="transaction">
+
+        {/* Transaction 2 */}
+        <div className="left">
+          <div className="name">Bonus</div>
+          <div className="description"></div>
+        </div>
+        <div className="right">
+          <div className="price green">+RM500</div>
+          <div className="datetime">2/12/2025 15:30</div>
+        </div>
+
+      </div>
+
+
+    </main>
+  )
+}
+
+export default App
